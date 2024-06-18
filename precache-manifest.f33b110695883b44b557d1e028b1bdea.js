5823bd97b562c8094e9c88559054dc6280f85599
Anjali Rambhad <anjalirambhad28@gmail.com>
2024-06-18 16:17:07 +0530

    Updates

---
+++ precache-manifest.f33b110695883b44b557d1e028b1bdea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d95f547732fb648684c227c11a1298a",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "e161141d36d493634af9",
+    "url": "/TextUtils-React/static/css/main.9d918695.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/TextUtils-React/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/TextUtils-React/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "e161141d36d493634af9",
+    "url": "/TextUtils-React/static/js/main.2e6e63a7.chunk.js"
+  },
+  {
+    "revision": "e8122ccafefab8254f73",
+    "url": "/TextUtils-React/static/js/runtime~main.bfec75dd.js"
+  }
+]);
